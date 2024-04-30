1265aa1590641af56c1eea52706b06d8802882ab
shu <g0988037401@gmail.com>
2024-04-30 15:15:31 +0800

    php日曆製作

--- pra07.php
+++ pra07.php
@@ -11,9 +11,11 @@
         }
         
         td{
-            border:1px solid black;
+            border:1.5px solid #613030;
             padding: 10px 13px;
             font-size:20px;
+            color:#613030;
+            
         }
 
        
@@ -31,6 +33,9 @@
 </ul>
 
 <?php
+$year=date("Y");
+echo "年份:".$year;
+echo "<br>";
 $month=4;
 echo "月份:".$month;
 echo "<br>";
@@ -41,6 +46,7 @@ $days=date("t",$firstDay);
 $lastDay=strtotime(date("Y-$month-$days"));
 echo "<br>";
 echo "最後一天是".date("Y-m-d",$lastDay);
+$birthday='2000-4-21';
 
 
 echo "<table>";
@@ -57,12 +63,29 @@ echo "</tr>";
 for($i=0;$i<6;$i++){
     echo "<tr>"; 
     for($j=0;$j<7;$j++){
+        $date=$year.'-'.$month.'-'.$i*7+$j-($firstWeekStarDay-1);
+        $replace=mb_substr($birthday,0,4);
+        $replaceTo=str_replace($replace,date("Y"),$birthday);
+        $spDate=strtotime($replaceTo);
+        $dateSec=strtotime($date);
         if($i==0 && $j>=$firstWeekStarDay){
-            echo "<td>";
+            if($spDate==$dateSec){
+                echo "<td style='background-color:#FF9797;font-weight:bolder;font: size 24px;color:#804040'>";
+            }else if(date("w",strtotime($date))==0 || date("w",strtotime($date))==6){
+                echo "<td style='background-color :	#FFD2D2'>";
+            }else{
+                echo "<td>";
+            }
             echo $i*7+$j-($firstWeekStarDay-1);
             echo "</td>";
         }else if($i>0){
-            echo "<td>";
+            if($spDate==$dateSec){
+                echo "<td style='background-color:#FF9797;font-weight:bolder;font: size 24px;color:#804040'>";
+            }else if(date("w",strtotime($date))==0 || date("w",strtotime($date))==6){
+                echo "<td style='background-color :	#FFD2D2'>";
+            }else{
+                echo "<td>";
+            }
             if($i*7+$j-($firstWeekStarDay-1)<=$days){
                 echo $i*7+$j-($firstWeekStarDay-1);
             }else{
@@ -77,6 +100,98 @@ for($i=0;$i<6;$i++){
     echo "</tr>";
 }
 echo "</table>";
+?>
+
+
+
+<br>
+    <style>
+        .block-table{
+            width: 357px;
+            display:flex;
+            flex-wrap:wrap;
+        }
+
+        .item{
+            margin-left:-1px;
+            margin-top:-1px;
+            display:inline-block;
+            width: 50px;
+            height: 50px;
+            border:1px solid #613030;
+            position:relative;
+            transition:all 0.3s;
+            background:white;
+        }
+
+        .item-header{
+            margin-left:-1px;
+            margin-top:-1px;
+            display:inline-block;
+            width:50px;
+            height:25px;
+            border:1px solid #613030;
+            text-align: center;
+            background-color: #E1C4C4; 
+            color:#613030;
+        }
+
+        .item:hover{
+            background:#EBD3E8;
+            transform:scale(1.3);
+            font-weight:bold;
+            color:#8080C0;
+            transition: all 0.3s;
+            z-index:10;
+        }
+
+        .holiday{
+            background:#FFD2D2;
+        }
+
+    </style>
+
+
+
+<?php
+
+
+$days=[];
+for($i=0;$i<42;$i++){
+    $diff=$i-$firstWeekStarDay;
+    $days[]=date("Y-m-d",strtotime("$diff days",$firstDay));
+}
+// echo "<pre>";
+// print_r($days);
+// echo "</pre>";
+echo "<div class='block-table'>";
+echo "<div class='item-header'>日</div>";
+echo "<div class='item-header'>一</div>";
+echo "<div class='item-header'>二</div>";
+echo "<div class='item-header'>三</div>";
+echo "<div class='item-header'>四</div>";
+echo "<div class='item-header'>五</div>";
+echo "<div class='item-header'>六</div>";
+
+
+foreach($days as $day){
+    $format=explode("-",$day)[2];
+    $w=date("w",strtotime($day));
+    if($w==0 || $w==6){
+
+        echo "<div class='item holiday'>$format</div>";
+    }else{
+
+        echo "<div class='item'>";
+        echo "<div class='date'>$format</div>";
+        echo "</div>";
+    }
+}
+echo "</div>";
+
+
+
+
 ?>
 
 
